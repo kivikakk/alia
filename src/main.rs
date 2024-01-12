1eabf50b685c6984aefea22ad08a223feaed9e55
Ashe Connor <ashe@kivikakk.ee>
2024-01-12 13:07:21 +1100

    main: LSP skeleton from rust-analyzer.
    https://github.com/rust-lang/rust-analyzer/blob/9d8889cdfcc3aa0302353fc988ed21ff9bc9925c/lib/lsp-server/examples/goto_def.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use lsp_server::Connection;
+use lsp_server::ExtractError;
+use lsp_server::Message;
+use lsp_server::Request;
+use lsp_server::RequestId;
+use lsp_server::Response;
+use lsp_types::request::GotoDefinition;
+use lsp_types::GotoDefinitionResponse;
+use lsp_types::InitializeParams;
+use lsp_types::OneOf;
+use lsp_types::ServerCapabilities;
+
+fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    eprintln!("alia lsp server starting");
+
+    let (connection, io_threads) = Connection::stdio();
+    let server_capabilities = serde_json::to_value(&ServerCapabilities {
+        definition_provider: Some(OneOf::Left(true)),
+        ..Default::default()
+    })
+    .unwrap();
+    let initialization_params = match connection.initialize(server_capabilities) {
+        Ok(it) => it,
+        Err(e) => {
+            if e.channel_is_disconnected() {
+                io_threads.join()?;
+            }
+            return Err(e.into());
+        }
+    };
+    main_loop(connection, initialization_params)?;
+    io_threads.join()?;
+
+    eprintln!("baibai");
+    Ok(())
+}
+
+fn main_loop(
+    connection: Connection,
+    params: serde_json::Value,
+) -> Result<(), Box<dyn Error + Sync + Send>> {
+    let _params: InitializeParams = serde_json::from_value(params).unwrap();
+    eprintln!("main_loop");
+    for msg in &connection.receiver {
+        eprintln!("msg: {msg:?}");
+        match msg {
+            Message::Request(req) => {
+                eprintln!("got request: {req:?}");
+                if connection.handle_shutdown(&req)? {
+                    return Ok(());
+                }
+                match cast::<GotoDefinition>(req) {
+                    Ok((id, params)) => {
+                        eprintln!("got gotoDefinition request #{id}: {params:?}");
+                        let result = Some(GotoDefinitionResponse::Array(Vec::new()));
+                        let result = serde_json::to_value(&result).unwrap();
+                        let resp = Response {
+                            id,
+                            result: Some(result),
+                            error: None,
+                        };
+                        connection.sender.send(Message::Response(resp))?;
+                        continue;
+                    }
+                    Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    Err(ExtractError::MethodMismatch(req)) => req,
+                };
+                // ...
+            }
+            Message::Response(resp) => {
+                eprintln!("got response: {resp:?}");
+            }
+            Message::Notification(not) => {
+                eprintln!("got notification: {not:?}");
+            }
+        }
+    }
+    Ok(())
+}
+
+fn cast<R>(req: Request) -> Result<(RequestId, R::Params), ExtractError<Request>>
+where
+    R: lsp_types::request::Request,
+    R::Params: serde::de::DeserializeOwned,
+{
+    req.extract(R::METHOD)
 }
