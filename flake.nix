ff8f8c2eb5614b8e8eb247db07284e395fecfb62
Ashe Connor <ashe@kivikakk.ee>
2024-01-12 12:43:46 +1100

    init.

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "alia";
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      inherit (pkgs) lib;
+      cargoToml = lib.importTOML ./Cargo.toml;
+    in rec {
+      formatter = pkgs.alejandra;
+
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        pname = cargoToml.package.name;
+        version = cargoToml.package.version;
+        src = ./.;
+
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+
+        buildInputs =
+          [
+            pkgs.openssl
+          ]
+          ++ lib.optionals (pkgs.stdenv.isDarwin) [
+            pkgs.darwin.apple_sdk.frameworks.Security
+          ];
+      };
+
+      devShells.default = packages.default.overrideAttrs (finalAttrs: prevAttrs: {
+        nativeBuildInputs =
+          prevAttrs.nativeBuildInputs
+          ++ [
+            pkgs.rust-analyzer
+            pkgs.rustfmt
+            pkgs.lldb_16
+          ];
+      });
+
+      nixosModules.default = {...}: {
+        home-manager.sharedModules = [(import ./home.nix {inherit self system;})];
+      };
+
+      darwinModules.default = {...}: {
+        home-manager.sharedModules = [(import ./home.nix {inherit self system;})];
+      };
+    });
+}
---
+++ home.nix
@@ -0,0 +1,60 @@
+{
+  self,
+  system,
+}: {
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.programs.alia;
+  inherit (lib) mkIf mkEnableOption mkOption mkMerge types;
+in {
+  options.programs.alia = {
+    enable = mkEnableOption "Enable alia";
+
+    package = mkOption {
+      type = types.package;
+      default = self.packages.${system}.default;
+      description = "Package to use for alia (defaults to this flake's).";
+    };
+
+    enableHelix = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Add alia to Helix's configuration";
+    };
+  };
+
+  config = mkIf (cfg.enable) (mkMerge [
+    {
+      home.packages = [cfg.package];
+    }
+    (mkIf (cfg.enableHelix) {
+      programs.helix.languages = {
+        language = [
+          {
+            name = "alia";
+            scope = "source.alia";
+            injection-regex = "alia";
+            file-types = ["lia"];
+            roots = ["make.lia"];
+            auto-format = true;
+            comment-token = ";";
+            language-servers = ["alia-lsp"];
+            indent = {
+              tab-width = 2;
+              unit = "  ";
+            };
+            diagnostic-severity = "Hint";
+            grammar = "clojure";
+            text-width = 100;
+          }
+        ];
+        language-server.alia-lsp = {
+          command = "${cfg.package}/bin/alia";
+          args = ["--lsp"];
+        };
+      };
+    })
+  ]);
+}
