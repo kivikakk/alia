5ae3827fd0f3ad9c3beb47a43382c1b3d9c3aafb
Ashe Connor <ashe@kivikakk.ee>
2024-01-12 13:58:15 +1100

    lsp: show a code action.

--- src/lsp.rs
+++ src/lsp.rs
@@ -1,16 +1,11 @@
 use std::error::Error;
 
-use lsp_server::Connection;
-use lsp_server::ExtractError;
-use lsp_server::Message;
-use lsp_server::Request;
-use lsp_server::RequestId;
-use lsp_server::Response;
-use lsp_types::request::GotoDefinition;
-use lsp_types::GotoDefinitionResponse;
-use lsp_types::InitializeParams;
-use lsp_types::OneOf;
-use lsp_types::ServerCapabilities;
+use lsp_server::{Connection, ExtractError, Message, Request, RequestId, Response, ResponseError};
+use lsp_types::request::{CodeActionRequest, GotoDefinition};
+use lsp_types::{
+    CodeActionResponse, Command, ExecuteCommandOptions, GotoDefinitionResponse, InitializeParams,
+    OneOf, ServerCapabilities,
+};
 
 pub(crate) fn main(args: Vec<String>) -> Result<(), Box<dyn Error + Send + Sync>> {
     eprintln!("alia lsp server starting");
@@ -22,6 +17,11 @@ pub(crate) fn main(args: Vec<String>) -> Result<(), Box<dyn Error + Send + Sync>
     let (connection, io_threads) = Connection::stdio();
     let server_capabilities = serde_json::to_value(&ServerCapabilities {
         definition_provider: Some(OneOf::Left(true)),
+        code_action_provider: Some(true.into()),
+        execute_command_provider: Some(ExecuteCommandOptions {
+            commands: vec!["command".to_string()],
+            ..Default::default()
+        }),
         ..Default::default()
     })
     .unwrap();
@@ -46,32 +46,54 @@ fn main_loop(
     params: serde_json::Value,
 ) -> Result<(), Box<dyn Error + Sync + Send>> {
     let _params: InitializeParams = serde_json::from_value(params).unwrap();
-    eprintln!("main_loop");
     for msg in &connection.receiver {
-        eprintln!("msg: {msg:?}");
         match msg {
             Message::Request(req) => {
-                eprintln!("got request: {req:?}");
                 if connection.handle_shutdown(&req)? {
                     return Ok(());
                 }
-                match cast::<GotoDefinition>(req) {
-                    Ok((id, params)) => {
-                        eprintln!("got gotoDefinition request #{id}: {params:?}");
+                let req = match cast::<GotoDefinition>(req) {
+                    Ok((id, _params)) => {
                         let result = Some(GotoDefinitionResponse::Array(Vec::new()));
                         let result = serde_json::to_value(&result).unwrap();
-                        let resp = Response {
+                        connection.sender.send(Message::Response(Response {
                             id,
                             result: Some(result),
                             error: None,
-                        };
-                        connection.sender.send(Message::Response(resp))?;
+                        }))?;
                         continue;
                     }
                     Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
                     Err(ExtractError::MethodMismatch(req)) => req,
                 };
-                // ...
+                let req = match cast::<CodeActionRequest>(req) {
+                    Ok((id, _params)) => {
+                        let result: Option<CodeActionResponse> = Some(vec![Command::new(
+                            "title".to_string(),
+                            "command".to_string(),
+                            None,
+                        )
+                        .into()]);
+                        let result = serde_json::to_value(&result).unwrap();
+                        connection.sender.send(Message::Response(Response {
+                            id,
+                            result: Some(result),
+                            error: None,
+                        }))?;
+                        continue;
+                    }
+                    Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    Err(ExtractError::MethodMismatch(req)) => req,
+                };
+                connection.sender.send(Message::Response(Response {
+                    id: req.id,
+                    result: None,
+                    error: Some(ResponseError {
+                        code: 1, // XXX
+                        message: "weh".to_string(),
+                        data: None,
+                    }),
+                }))?;
             }
             Message::Response(resp) => {
                 eprintln!("got response: {resp:?}");
