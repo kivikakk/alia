0f32c278d3444dc41df8fdaa87f1bfe390efab65
Amelia Cuss <amelia@kivikakk.ee>
2024-01-24 15:23:43 +1100

    builtins: add eval.

--- src/vm/builtins.rs
+++ src/vm/builtins.rs
@@ -1,3 +1,5 @@
+use crate::{compiler::compile_one, parser::Node};
+
 use super::{proc::Proc, Module, Val, Vm};
 
 fn eval_args(vm: &mut Vm, proc: &mut Proc, args: Vec<&Val>) -> Vec<Val> {
@@ -8,6 +10,7 @@ pub(super) fn add_all(vm: &mut Vm, m: &mut Module) {
     m.add_bind_builtin(vm, "print", print);
     m.add_bind_builtin(vm, "quote", quote);
     m.add_bind_builtin(vm, "set", set);
+    m.add_bind_builtin(vm, "eval", eval);
 }
 
 fn print(vm: &mut Vm, proc: &mut Proc, args: Vec<&Val>) -> Val {
@@ -43,4 +46,24 @@ fn set(vm: &mut Vm, proc: &mut Proc, args: Vec<&Val>) -> Val {
     let v = proc.eval(vm, args[1]);
     _ = proc.module.borrow_mut().binds.insert(*s, v.clone());
     v
+}
+
+fn eval(vm: &mut Vm, proc: &mut Proc, args: Vec<&Val>) -> Val {
+    // (eval "print") => builtins/print
+
+    if args.len() != 1 {
+        panic!("eval should take one argument");
+    }
+
+    let s = match args[0] {
+        Val::String(s) => s,
+        _ => panic!("trying to eval {}", args[0].format(vm)),
+    };
+    let node = match s.parse::<Node>() {
+        Ok(node) => node,
+        _ => panic!("failed to parse {s:?}"),
+    };
+    let code = compile_one(&node).unwrap();
+    let mut stack = vm.run_to_completion(proc.module.clone(), code);
+    stack.pop().unwrap()
 }
